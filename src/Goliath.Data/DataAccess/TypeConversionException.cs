a55b60126eef28db80028a1b1902b2d7fdefedcb
hamsman <hamsman@localhost>
2011-07-10 20:30:06 -0400

    type conversion

---
+++ src/Goliath.Data/DataAccess/TypeConversionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Goliath.Data.DataAccess
+{
+    public class TypeConversionException : DataAccessException
+    {
+        public TypeConversionException(Type from, Type to)
+            : base("Could not convert {0} to {1}", from, to)
+        {
+        }
+
+        public TypeConversionException(Type from, Type to, Exception innerException)
+            : base(string.Format("Could not convert {0} to {1}", from, to), innerException)
+        {
+        }
+    }
+}
