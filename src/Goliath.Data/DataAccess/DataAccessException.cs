726c55b4152baf5e5565f2418a93b164c2375459
hamsman <hamsman@localhost>
2011-07-14 09:17:38 -0400

    sql statement builder

--- src/Goliath.Data/DataAccess/DataAccessException.cs
+++ src/Goliath.Data/DataAccess/DataAccessException.cs
@@ -10,8 +10,18 @@ namespace Goliath.Data
     /// </summary>
     public class DataAccessException : GoliathDataException
     {
-        public DataAccessException(string messageFormat, params object[] args) : base(string.Format(messageFormat, args)) { } 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataAccessException"/> class.
+        /// </summary>
+        /// <param name="messageFormat">The message format.</param>
+        /// <param name="args">The args.</param>
+        public DataAccessException(string messageFormat, params object[] args) : base(string.Format(messageFormat, args)) { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataAccessException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="innerException">The inner exception.</param>
         public DataAccessException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
---
+++ src/Goliath.Data/Sql/Enums.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Goliath.Data.Sql
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    enum JoinType
+    {
+        Inner = 1,
+        Left = 2,
+        Right = 4,
+        Full = 8
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    enum ComparisonOperator
+    {
+        Equals = 0,
+        NotEquals,
+        Like,
+        NotLike,
+        GreaterThan,
+        GreaterOrEquals,
+        LessThan,
+        LessOrEquals,
+        In
+    }
+
+    enum SortType
+    {
+        Ascending,
+        Descinding,
+    }
+}
