213bcc84250eec7bb38bbdd167d68274445996fb
hamsman <hamsman@localhost>
2011-08-07 13:58:19 -0400

    comment added

--- src/Goliath.Data/Generators/KeyGeneratorStore.cs
+++ src/Goliath.Data/Generators/KeyGeneratorStore.cs
@@ -6,8 +6,16 @@ using System.Text;
 
 namespace Goliath.Data.Mapping
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class KeyGeneratorStore : KeyedCollectionBase<string, IKeyGenerator>, IKeyGeneratorStore
     {
+        /// <summary>
+        /// Gets the key for item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <returns></returns>
         protected override string GetKeyForItem(IKeyGenerator item)
         {
             return item.Name;
