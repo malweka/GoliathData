3daee9731c369e5b485095e319fb020b1078a02e
Emmanuel <348391+malweka@users.noreply.github.com>
2011-09-15 19:30:50 -0400

    sql files

---
+++ src/Goliath.Data.Console/Scripts/Mssql2008/WebZooAutoIncrement/01_animals.sql
@@ -0,0 +1,9 @@
+﻿Create table animals(
+	Id int primary key not null identity,
+	Name nvarchar(50) not null,
+	Age float not null,
+	Location char(10),
+	ReceivedOn datetime not null,
+	ZooId int not null,
+	foreign key(ZooId) references zoos(Id)
+);
