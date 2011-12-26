bf135beb8b95e1c78eb541b1fddefc33cdc0e143
malweka <malweka@localhost>
2011-12-26 16:27:04 -0500

    class template modification to implement IEquatable and Equals override

--- src/Goliath.Data.Console/Generated/Employee.cs
+++ src/Goliath.Data.Console/Generated/Employee.cs
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////
 //	
-//	Auto generated -  Class Template 1.1.0
+//	Auto generated -  Class Template 1.2.0
 //
 ///////////////////////////////////////////////////////////////////
 using System;
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 
 namespace WebZoo.Data
 {
-	public partial class Employee : WebZoo.Data.BaseEntityInt
+	public partial class Employee : WebZoo.Data.BaseEntityInt, IEquatable<Employee>
 	{
 		#region Primary Key
 
@@ -33,6 +33,51 @@ namespace WebZoo.Data
 		public virtual IList<WebZoo.Data.Animal> AnimalsOnAnimalsHandler_EmployeeId { get; set; }
 
 		#endregion
+		
+		#region Equatable
+		
+		/// <summary>
+        /// Determines whether the specified <see cref="Employee"/> is equal to this instance.
+        /// </summary>
+        /// <param name="other">The <see cref="Employee"/> to compare with this instance.</param>
+        /// <returns>
+        /// 	<c>true</c> if the specified <see cref="Employee"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+		public bool Equals(Employee other)
+		{
+			if(other == null)
+				return false;
+			
+			return other.Id.Equals(Id);
+		}
+		
+		/// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+		public override int GetHashCode()
+        {
+        	return Id.GetHashCode();
+        }
+		
+		/// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with this instance.</param>
+        /// <returns>
+        /// 	<c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
+		public override bool Equals(object obj)
+        {
+            if (obj is Employee)
+                return Equals((Employee)obj);
+		
+            return base.Equals(obj);
+		}
+		#endregion
+		
 	}
 
 	#region metadata
