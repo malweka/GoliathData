0d170d951235c89f190da3b8c0e1cc9022e6a1d9
Emmanuel <348391+malweka@users.noreply.github.com>
2012-02-24 18:47:15 -0500

    adding tests

--- src/Goliath.Data.Tests/AssemblySetupTestFixture.cs
+++ src/Goliath.Data.Tests/AssemblySetupTestFixture.cs
@@ -24,13 +24,23 @@ namespace Goliath.Data.Tests
         public void SetUp()
         {
             string pdir = AppDomain.CurrentDomain.BaseDirectory.Substring(0, AppDomain.CurrentDomain.BaseDirectory.IndexOf("src"));
-            string mappingFolder = Path.Combine(pdir, "src", "Goliath.Data.Console", "Generated", "Sqlite");
-            string dbfile = Path.Combine(pdir, "src", "Goliath.Data.Console", "Data", "WebZoo.db");
+
+            string autoIncrementFileName = "ZooDb_auto_increment.db";
+            //string guidFileName = string.Format("ZooDb_guid.{0}.db",  DateTime.Now.Ticks);
+
+            string mappingFolder = Path.Combine(pdir, "src", "Goliath.Data.Tests", "Entities", "AutoIncrement");
+            string scriptFolder = Path.Combine(pdir, "src", "Goliath.Data.Tests", "Scripts", "Sqlite", "AutoIncrement");
+            string dbfile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, autoIncrementFileName);
             string cs = string.Format("Data Source={0}; Version=3", dbfile);
 
+            
             string mapfile = Path.Combine(mappingFolder, MapFileName);
 
-            var sessionFactory = new Database().Configure(mapfile, cs)
+            MapConfig config = MapConfig.Create(mapfile);
+            config.Settings.ConnectionString = cs;
+            DatabaseInit.CreateSqliteDatabase(config, scriptFolder, dbfile);
+
+            var sessionFactory = new Database().Configure(config)
                 .Provider(new SqliteProvider()).Init();
 
             SessionHelper.SetFactory(sessionFactory);
@@ -40,6 +50,17 @@ namespace Goliath.Data.Tests
     public class SessionHelper
     {
         public static ISessionFactory Factory { get; private set; }
+
+        public static string BaseDirectory
+        {
+            get
+            {
+                string pdir = AppDomain.CurrentDomain.BaseDirectory.Substring(0, AppDomain.CurrentDomain.BaseDirectory.IndexOf("src"));
+                string basefolder = Path.Combine(pdir, "src", "Goliath.Data.Tests");
+                return basefolder;
+            }
+        }
+
 
         public static void SetFactory(ISessionFactory factory)
         {
