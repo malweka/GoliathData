73472a0c8b3aa39eeab71052293220b3a5ef68f2
malweka <malweka@localhost>
2012-02-24 22:12:39 -0500

    unit test additions

--- src/Goliath.Data.Tests/MapConfigTests.cs
+++ src/Goliath.Data.Tests/MapConfigTests.cs
@@ -14,13 +14,23 @@ namespace Goliath.Data.Tests
     public class MapConfigTests
     {
         [Test]
-        public void Load_load_simple_xml_config_should_have_valid_map_config()
+        public void Load_simple_xml_config_should_have_valid_map_config()
         {
             string testMapfile = Path.Combine(SessionHelper.BaseDirectory, "TestFiles", "Test001.data.xml");
             MapConfig config = new MapConfig();
             config.Load(testMapfile);
 
             Assert.AreEqual(2, config.EntityConfigs.Count);
+        }
+
+        [Test, ExpectedException(typeof(MappingException))]
+        public void Load_xml_config_with_entity_non_existing_map_should_throw()
+        {
+            string testMapfile = Path.Combine(SessionHelper.BaseDirectory, "TestFiles", "Test002.data.xml");
+            MapConfig config = new MapConfig();
+            config.Load(testMapfile);
+
+            Assert.Fail("should have thrown");
         }
     }
 }
