2714cb27103a6dd52bb79c98b203d5d5357cd06d
malweka <malweka@localhost>
2012-06-02 17:57:50 -0400

    statement text parsing

---
+++ src/Goliath.Data/CompiledStatement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Goliath.Data
+{
+    class CompiledStatement
+    {
+        public string Body { get; set; }
+
+        Dictionary<string, string> paramPropertyMap = new Dictionary<string, string>();
+
+        public Dictionary<string, string> ParamPropertyMap
+        {
+            get { return paramPropertyMap; }
+        }
+    }
+}
