39d584f6f9a66fc1b7ef538d0e160faf45aea02a
malweka <malweka@localhost>
2012-06-11 09:16:20 -0400

    new fasterflect addition

--- src/Goliath.Data/DataAccess/GetSetStore.cs
+++ src/Goliath.Data/DataAccess/GetSetStore.cs
@@ -2,7 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Reflection;
-using Fasterflect;
+using Goliath.Fasterflect;
 
 namespace Goliath.Data.DataAccess
 {
