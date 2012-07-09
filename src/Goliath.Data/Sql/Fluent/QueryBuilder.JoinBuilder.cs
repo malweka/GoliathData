91633847bd7893383c33f71160ff28f5584d1065
malweka <malweka@localhost>
2012-07-09 19:39:22 -0400

    querybuilder implementation

---
+++ src/Goliath.Data/Sql/Fluent/QueryBuilder.JoinBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Goliath.Data.Sql
+{
+    partial class QueryBuilder : IJoinable
+    {
+        public IJoinable InnerJoin(string tableName, string alias)
+        {
+            var join = BuildJoin(tableName, alias, JoinType.Inner);
+            joins.Add(alias, join);
+            return join;
+        }
+
+        public IJoinable LeftJoin(string tableName, string alias)
+        {
+            var join = BuildJoin(tableName, alias, JoinType.Left);
+            joins.Add(alias, join);
+            return join;
+        }
+
+        public IJoinable RightJoin(string tableName, string alias)
+        {
+            var join = BuildJoin(tableName, alias, JoinType.Right);
+            joins.Add(alias, join);
+            return join;
+        }
+        
+        IJoinable BuildJoin(string joinTableName, string jointTableAlias, JoinType type)
+        {
+            JoinBuilder join = new JoinBuilder(this, this.tableName, joinTableName, jointTableAlias) { Type = type };
+            return join;
+        }
+
+
+        #region IJoinQueryBuilder Members
+
+        public IWhereOnRelation ForJoin(string alias)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IJoinable Members
+
+        IJoinOperation IJoinable.On(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
