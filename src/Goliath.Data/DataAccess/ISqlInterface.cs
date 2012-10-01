803c691a2f4c7a8e0b07a4c5457ceb3bbd3a9e06
malweka <malweka@localhost>
2012-09-30 20:03:37 -0400

    adding execute method to session

--- src/Goliath.Data/DataAccess/ISqlInterface.cs
+++ src/Goliath.Data/DataAccess/ISqlInterface.cs
@@ -13,6 +13,8 @@ namespace Goliath.Data
     /// </summary>
     public interface ISqlInterface
     {
+        #region queries
+
         /// <summary>
         /// Selects all.
         /// </summary>
@@ -42,6 +44,46 @@ namespace Goliath.Data
         /// <param name="columns">The columns.</param>
         /// <returns></returns>
         ITableNameBuilder Select(string column, params string[] columns);
+
+        #endregion
         
+        /// <summary>
+        /// Updates the specified entity.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        int Update<T>(T entity);
+
+        /// <summary>
+        /// Inserts the specified entity.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <param name="recursive">if set to <c>true</c> [recursive].</param>
+        /// <returns></returns>
+        int Insert<T>(T entity, bool recursive = false);
+
+        /// <summary>
+        /// Deletes the specified entity.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <param name="cascade">if set to <c>true</c> [cascade].</param>
+        /// <returns></returns>
+        int Delete<T>(T entity, bool cascade = false);
+
+        #region Run commands
+
+        IList<T> RunList<T>(SqlQueryBody sql, int limit, int offset, params QueryParam[] paramArray);
+        IList<T> RunList<T>(SqlQueryBody sql, int limit, int offset, out long total, params QueryParam[] paramArray);
+        IList<T> RunList<T>(string sql, params QueryParam[] paramArray);
+        IList<T> RunList<T>(SqlQueryBody sql, params QueryParam[] paramArray);
+
+        T Run<T>(ISession session, string sql, params QueryParam[] paramArray);
+        T Run<T>(ISession session, SqlQueryBody sql, params QueryParam[] paramArray);
+
+        #endregion
+
     }
 }
