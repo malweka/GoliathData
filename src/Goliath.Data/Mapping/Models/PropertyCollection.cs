644251d23cb24c6543d1a03180648eaa771aa9c6
Emmanuel <348391+malweka@users.noreply.github.com>
2013-02-23 00:54:30 -0500

    templating and code generation from reflection

--- src/Goliath.Data/Mapping/Models/PropertyCollection.cs
+++ src/Goliath.Data/Mapping/Models/PropertyCollection.cs
@@ -22,35 +22,4 @@ namespace Goliath.Data.Mapping
          return item.PropertyName;
       }
    }
-
-   /// <summary>
-   /// 
-   /// </summary>
-   [Serializable]
-   [CollectionDataContract]
-   public class RelationCollection : KeyedCollectionBase<string, Relation>
-   {
-       /// <summary>
-       /// Inserts the item.
-       /// </summary>
-       /// <param name="index">The index.</param>
-       /// <param name="item">The item.</param>
-       protected override void InsertItem(int index, Relation item)
-       {
-           item.InternalIndex = index;
-           base.InsertItem(index, item);
-       }
-
-       /// <summary>
-       /// When implemented in a derived class, extracts the key from the specified element.
-       /// </summary>
-       /// <param name="item">The element from which to extract the key.</param>
-       /// <returns>
-       /// The key for the specified element.
-       /// </returns>
-      protected override string GetKeyForItem(Relation item)
-      {
-         return item.PropertyName;
-      }
-   }
 }
