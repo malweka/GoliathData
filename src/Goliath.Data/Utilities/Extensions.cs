caddfa7819593a99f696f5b921b7f09650d07395
Emmanuel <348391+malweka@users.noreply.github.com>
2013-06-09 11:18:48 -0400

    check null before accessing position

--- src/Goliath.Data/Utilities/Extensions.cs
+++ src/Goliath.Data/Utilities/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Goliath.Data.Utils
 {
@@ -14,9 +15,12 @@ namespace Goliath.Data.Utils
       /// <returns></returns>
       public static string ConvertToString(this Stream stream)
       {
-         stream.Position = 0;
+          if (stream == null) throw new ArgumentNullException("stream");
+
+          stream.Position = 0;
          System.IO.StreamReader reader = new StreamReader(stream, true);
          string text = reader.ReadToEnd();
+         
          return text;
       }
 
