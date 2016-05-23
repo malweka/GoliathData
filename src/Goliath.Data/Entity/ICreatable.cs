aae4ba47446405812b73e90cfa69f6d00aaffc51
malweka <malweka@localhost>
2016-05-23 01:28:53 -0400

    upgrade packages

---
+++ src/Goliath.Data/Entity/ICreatable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Goliath.Data.Entity
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ICreatable
+    {
+        /// <summary>
+        /// Gets or sets the created on.
+        /// </summary>
+        /// <value>
+        /// The created on.
+        /// </value>
+        DateTime CreatedOn { get; set; }
+        /// <summary>
+        /// Gets or sets the created by.
+        /// </summary>
+        /// <value>
+        /// The created by.
+        /// </value>
+        string CreatedBy { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IModifiable
+    {
+        /// <summary>
+        /// Gets or sets the modified on.
+        /// </summary>
+        /// <value>
+        /// The modified on.
+        /// </value>
+        DateTime? ModifiedOn { get; set; }
+        /// <summary>
+        /// Gets or sets the modified by.
+        /// </summary>
+        /// <value>
+        /// The modified by.
+        /// </value>
+        string ModifiedBy { get; set; }
+    }
+}
