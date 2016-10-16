474f3db71aec655984a23e42fb3d7d27e79ac845
Emmanuel <348391+malweka@users.noreply.github.com>
2016-10-16 01:21:15 -0400

    build script

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -1,10 +1,10 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyCompany("Hamsman")]
 [assembly: AssemblyProduct("Goliath")]
 [assembly: AssemblyCopyright("Copyright © Emmanuel Hameyie 2009-2016")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("1.3.2.2")]
-[assembly: AssemblyFileVersion("1.3.2.2")]
+[assembly: AssemblyVersion("1.4.1.8")]
+[assembly: AssemblyFileVersion("1.4.1.8")]
 
