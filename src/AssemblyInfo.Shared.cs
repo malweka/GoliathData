531e2146d6d20b09ec20c92dfe50305e3d3456a2
malweka <malweka@localhost>
2016-12-10 11:34:32 -0500

    build 1.4.1.16

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -5,6 +5,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Emmanuel Hameyie 2009-2016")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("1.4.1.16")]
-[assembly: AssemblyFileVersion("1.4.1.16")]
+[assembly: AssemblyVersion("1.4.1.17")]
+[assembly: AssemblyFileVersion("1.4.1.17")]
 
