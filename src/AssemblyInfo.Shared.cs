35f91bfb2b530522f5a8a85c0f6b41edbeb3fb85
Emmanuel Hameyie <hamsman@gmail.com>
2019-01-16 20:21:07 -0500

    updating dependencies

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -5,6 +5,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Emmanuel Hameyie 2009-2018")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("1.5.0.24")]
-[assembly: AssemblyFileVersion("1.5.0.24")]
+[assembly: AssemblyVersion("1.5.0.25")]
+[assembly: AssemblyFileVersion("1.5.0.25")]
 
--- src/codegen_merge.cmd
+++ src/codegen_merge.cmd
@@ -1,6 +1,6 @@
 set CWD=%~dp0
 cd Build\CodeGen\tools
-C:\Tools\ILRepack.exe /wildcards /verbose /out:GoliathData_merge.exe GoliathData.exe *.dll
+C:\Tools\ILRepack.exe /wildcards /internalize /verbose /out:GoliathData_merge.exe GoliathData.exe *.dll
 MOVE GoliathData_merge.exe  %CWD%\Build\GoliathData.exe
 cd %CWD%\Build
 pause
