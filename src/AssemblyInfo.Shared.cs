8ba6b6188b1088ea5ef52db058ee85692b698b89
Emmanuel Hameyie <hamsman@gmail.com>
2019-03-01 23:23:48 -0500

    version update

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -5,6 +5,6 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Emmanuel Hameyie 2009-2018")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("1.5.1.2")]
-[assembly: AssemblyFileVersion("1.5.1.2")]
+[assembly: AssemblyVersion("1.6.0.17")]
+[assembly: AssemblyFileVersion("1.6.0.17")]
 
